83b680bcdbf636bfb6bc42e06c03f04859dc85a2
Leo Goodstadt <src@llew.org.uk>
2009-11-22 18:39:58 +0000

    bug in get_job_names for jobs with no parameters. jobSignalledBreak was not exported
    --HG--
    extra : convert_revision : svn%3Aa4fb1d42-34dc-11de-8ef0-0f53c1b7e458/trunk%40219

--- src/ruffus/__init__.py
+++ src/ruffus/__init__.py
@@ -26,7 +26,7 @@
 #################################################################################
 #from graph import *
 #from print_dependencies import *
-from task import pipeline_printout, pipeline_printout_graph, pipeline_run, register_cleanup, check_if_uptodate, split, transform, merge, collate, files, files_re, follows, parallel, stderr_logger, black_hole_logger, suffix, regex, inputs, touch_file, combine, mkdir, output_from, posttask
+from task import pipeline_printout, pipeline_printout_graph, pipeline_run, register_cleanup, check_if_uptodate, split, transform, merge, collate, files, files_re, follows, parallel, stderr_logger, black_hole_logger, suffix, regex, inputs, touch_file, combine, mkdir, output_from, posttask, JobSignalledBreak
 #output_dependency_tree_in_dot_format, output_dependency_tree_key_in_dot_format 
 import ruffus_version
 __version__=ruffus_version.__version
