f474158b7a8d53fc976d8bfb7ca9a571b24d5d00
bunbun68 <none@none>
2010-07-15 10:26:16 +0000

    --HG-- extra : convert_revision : svn%3Aa4fb1d42-34dc-11de-8ef0-0f53c1b7e458/trunk%40278

--- ruffus/test/test_N_x_M_and_collate.py
+++ ruffus/test/test_N_x_M_and_collate.py
@@ -2,43 +2,43 @@
 """
 
     test_N_x_M_and_collate.py
-    
-    
+
+
         This script takes N pairs of input file pairs
                                 (with the suffices .gene and .gwas)
                           and runs them against M sets of simulation data
                                 (with the suffix .simulation)
         A summary per input file pair is then produced
-                    
-        
+
+
     In pseudo-code:
-        
+
         STEP_1:
-        
+
         for n_file in NNN_pairs_of_input_files:
             for m_file in MMM_simulation_data:
 
                 [n_file.gene,
                  n_file.gwas,
                  m_file.simulation] -> n_file.m_file.simulation_res
-                 
+
 
         STEP_2:
-        
+
         for n_file in NNN_pairs_of_input_files:
-            
+
             n_file.*.simulation_res -> n_file.mean
-            
+
 
         n = CNT_GENE_GWAS_FILES
         m = CNT_SIMULATION_FILES
-            
-        
+
+
 
 """
 
-CNT_GENE_GWAS_FILES     = 10
-CNT_SIMULATION_FILES    = 5
+CNT_GENE_GWAS_FILES     = 2
+CNT_SIMULATION_FILES    = 3
 
 
 
@@ -46,8 +46,8 @@ import os, sys
 exe_path = os.path.split(os.path.abspath(sys.argv[0]))[0]
 sys.path.insert(0, os.path.abspath(os.path.join(exe_path,"..", "..")))
 
-    
-    
+
+
 from ruffus import *
 import random
 from itertools import izip
@@ -55,7 +55,7 @@ from itertools import izip
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
-#   options        
+#   options
 
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
@@ -71,34 +71,34 @@ parser.add_option("-k", "--keep", dest = "keep",
 parser.add_option("-t", "--target_tasks", dest="target_tasks",
                   action="append",
                   default = ["statistical_summary"],
-                  metavar="JOBNAME", 
+                  metavar="JOBNAME",
                   type="string",
                   help="Target task(s) of pipeline.")
 parser.add_option("-f", "--forced_tasks", dest="forced_tasks",
                   action="append",
                   default = list(),
-                  metavar="JOBNAME", 
+                  metavar="JOBNAME",
                   type="string",
                   help="Pipeline task(s) which will be included even if they are up to date.")
 parser.add_option("-j", "--jobs", dest="jobs",
                   default=5,
-                  metavar="jobs", 
+                  metavar="jobs",
                   type="int",
                   help="Specifies the number of jobs (commands) to run simultaneously.")
 
 parser.add_option("-g", "--gene_data_dir", dest="gene_data_dir",
                   default="%s/temp_gene_data_for_intermediate_example" % exe_path,
-                  metavar="PATH", 
+                  metavar="PATH",
                   type="string",
                   help="Directory with gene data [*.genes / *.gwas].")
 parser.add_option("-s", "--simulation_data_dir", dest="simulation_data_dir",
                   default="%s/temp_simulation_data_for_intermediate_example" % exe_path,
-                  metavar="PATH", 
+                  metavar="PATH",
                   type="string",
                   help="Directory with simulation data [*.simulation].")
 parser.add_option("-w", "--working_dir", dest="working_dir",
                   default="%s/working_dir_for_intermediate_example" % exe_path,
-                  metavar="PATH", 
+                  metavar="PATH",
                   type="string",
                   help="Working directory.")
 
@@ -107,12 +107,12 @@ parser.add_option("-v", "--verbose", dest = "verbose",
                   action="count", default=0,
                   help="Print more verbose messages for each additional verbose level.")
 parser.add_option("-d", "--dependency", dest="dependency_file",
-                  metavar="FILE", 
+                  metavar="FILE",
                   type="string",
                   help="Print a dependency graph of the pipeline that would be executed "
                         "to FILE, but do not execute it.")
 parser.add_option("-F", "--dependency_graph_format", dest="dependency_graph_format",
-                  metavar="FORMAT", 
+                  metavar="FORMAT",
                   type="string",
                   default = 'svg',
                   help="format of dependency graph file. Can be 'ps' (PostScript), "+
@@ -129,7 +129,7 @@ parser.add_option("-n", "--just_print", dest="just_print",
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
-#   imports        
+#   imports
 
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
@@ -156,24 +156,24 @@ def get_gene_gwas_file_pairs(  ):
     """
     Helper function to get all *.gene, *.gwas from the direction specified
         in --gene_data_dir
-        
-    Returns 
-        file pairs with both .gene and .gwas extensions, 
+
+    Returns
+        file pairs with both .gene and .gwas extensions,
         corresponding roots (no extension) of each file
     """
-    
+
 
     gene_files = glob.glob(os.path.join(options.gene_data_dir, "*.gene"))
     gwas_files = glob.glob(os.path.join(options.gene_data_dir, "*.gwas"))
-    
+
     common_roots = set(map(lambda x: os.path.splitext(os.path.split(x)[1])[0], gene_files))
     common_roots &=set(map(lambda x: os.path.splitext(os.path.split(x)[1])[0], gwas_files))
     common_roots = list(common_roots)
-        
+
     p = os.path; g_dir = options.gene_data_dir
-    
+
     file_pairs = [[p.join(g_dir, x + ".gene"), p.join(g_dir, x + ".gwas")] for x in common_roots]
-    
+
     return file_pairs, common_roots
 
 #_________________________________________________________________________________________
@@ -185,8 +185,8 @@ def get_simulation_files(  ):
     """
     Helper function to get all *.simulation from the direction specified
         in --simulation_data_dir
-        Returns 
-            file with .simulation extensions, 
+        Returns
+            file with .simulation extensions,
             corresponding roots (no extension) of each file
     """
     simulation_files = glob.glob(os.path.join(options.simulation_data_dir, "*.simulation"))
@@ -219,13 +219,13 @@ working_dir = options.working_dir
 
 
 #_________________________________________________________________________________________
-# 
+#
 #   setup_simulation_data
-# 
+#
 #_________________________________________________________________________________________
 
-# 
-# mkdir: makes sure output directories exist before task    
+#
+# mkdir: makes sure output directories exist before task
 #
 @follows(mkdir(options.gene_data_dir, options.simulation_data_dir))
 def setup_simulation_data ():
@@ -248,13 +248,13 @@ def setup_simulation_data ():
 
 
 #_________________________________________________________________________________________
-# 
+#
 #   cleanup_simulation_data
-# 
+#
 #_________________________________________________________________________________________
 def try_rmdir (d):
     if os.path.exists(d):
-        try:    
+        try:
             os.rmdir(d)
         except OSError:
             sys.stderr.write("Warning:\t%s is not empty and will not be removed.\n" % d)
@@ -265,27 +265,27 @@ def cleanup_simulation_data ():
     """
     if options.verbose:
         sys.stderr.write("Cleanup working directory and simulation files.\n")
-    
-    #   
+
+    #
     #   cleanup gene and gwas files
-    # 
+    #
     for f in glob.glob(os.path.join(options.gene_data_dir, "*.gene")):
         os.unlink(f)
     for f in glob.glob(os.path.join(options.gene_data_dir, "*.gwas")):
         os.unlink(f)
     try_rmdir(options.gene_data_dir)
 
-    #   
+    #
     #   cleanup simulation
-    # 
+    #
     for f in glob.glob(os.path.join(options.simulation_data_dir, "*.simulation")):
         os.unlink(f)
     try_rmdir(options.simulation_data_dir)
-                
 
-    #   
+
+    #
     #   cleanup working_dir
-    # 
+    #
     for f in glob.glob(os.path.join(working_dir, "simulation_results", "*.simulation_res")):
         os.unlink(f)
     try_rmdir(os.path.join(working_dir, "simulation_results"))
@@ -298,7 +298,7 @@ def cleanup_simulation_data ():
 #_________________________________________________________________________________________
 #
 #   Step 1:
-#       
+#
 #        for n_file in NNN_pairs_of_input_files:
 #            for m_file in MMM_simulation_data:
 #
@@ -309,30 +309,30 @@ def cleanup_simulation_data ():
 #_________________________________________________________________________________________
 def generate_simulation_params ():
     """
-    Custom function to generate 
+    Custom function to generate
     file names for gene/gwas simulation study
     """
 
     simulation_files, simulation_file_roots    = get_simulation_files()
     gene_gwas_file_pairs, gene_gwas_file_roots =  get_gene_gwas_file_pairs()
-    
+
     for sim_file, sim_file_root in izip(simulation_files, simulation_file_roots):
         for (gene, gwas), gene_file_root in izip(gene_gwas_file_pairs, gene_gwas_file_roots):
-            
+
             result_file = "%s.%s.simulation_res" % (gene_file_root, sim_file_root)
             result_file_path = os.path.join(working_dir, "simulation_results", result_file)
-            
+
             yield [gene, gwas, sim_file], result_file_path, gene_file_root, sim_file_root, result_file
 
-# 
-# mkdir: makes sure output directories exist before task    
+#
+# mkdir: makes sure output directories exist before task
 #
 @follows(mkdir(options.working_dir, os.path.join(working_dir, "simulation_results")))
 @files(generate_simulation_params)
 def gwas_simulation(input_files, result_file_path, gene_file_root, sim_file_root, result_file):
     """
     Dummy calculation of gene gwas vs simulation data
-    Normally runs in parallel on a computational cluster       
+    Normally runs in parallel on a computational cluster
     """
     (gene_file,
     gwas_file,
@@ -345,13 +345,13 @@ def gwas_simulation(input_files, result_file_path, gene_file_root, sim_file_root
 #_________________________________________________________________________________________
 #
 #   Step 2:
-#       
+#
 #       Statistical summary per gene/gwas file pair
-# 
+#
 #        for n_file in NNN_pairs_of_input_files:
 #            working_dir/simulation_results/n.*.simulation_res
 #               -> working_dir/n.mean
-#       
+#
 #_________________________________________________________________________________________
 
 
@@ -370,16 +370,16 @@ def statistical_summary (result_files, summary_file):
 
 
 
-    
-    
-    
-    
-#888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888    
+
+
+
+
+#888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 #
 #   print pipeline or run pipeline
-# 
+#
 
-# 
+#
 #   Necessary to protect the "entry point" of the program under windows.
 #       see: http://docs.python.org/library/multiprocessing.html#multiprocessing-programming
 #
@@ -390,7 +390,7 @@ if __name__ == '__main__':
                 options.target_tasks.append([statistical_summary])
             pipeline_run([setup_simulation_data], [setup_simulation_data], multiprocess = options.jobs, verbose = 0)
         else:
-            if (not len(get_gene_gwas_file_pairs(  )[0]) or 
+            if (not len(get_gene_gwas_file_pairs(  )[0]) or
                 not len (get_simulation_files(  )[0])):
                 print "Warning!!\n\n\tNo *.gene / *.gwas or *.simulation: Run --debug to create simulation files first\n\n"
                 sys.exit(1)
@@ -398,13 +398,13 @@ if __name__ == '__main__':
 
         if options.just_print:
             pipeline_printout(sys.stdout, options.target_tasks, options.forced_tasks, verbose=options.verbose)
-        
+
         elif options.dependency_file:
             graph_printout (     open(options.dependency_file, "w"),
                                  options.dependency_graph_format,
-                                 options.target_tasks, 
+                                 options.target_tasks,
                                  options.forced_tasks)
-        else:    
+        else:
             pipeline_run(options.target_tasks, options.forced_tasks, multiprocess = options.jobs, verbose = options.verbose)
 
 
