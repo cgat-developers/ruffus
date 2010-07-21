95078a5f4890ecd006ec38e4ad3e7c85f0bee8d3
Leo Goodstadt <src@llew.org.uk>
2010-07-21 21:42:53 +0000

    add images and python code for generating them
    --HG--
    extra : convert_revision : svn%3Aa4fb1d42-34dc-11de-8ef0-0f53c1b7e458/trunk%40292

---
+++ doc/decorators/collate_ex.rst
@@ -0,0 +1,135 @@
+.. include:: ../global.inc
+.. _decorators.collate_ex:
+.. index:: 
+    pair: @collate (Advanced Usage); Syntax
+    pair: @collate, inputs(...); Syntax
+    pair: @collate, add_inputs(...); Syntax
+
+See :ref:`Decorators <decorators>` for more decorators
+
+####################################################
+@collate  with ``add_inputs`` and ``inputs``
+####################################################
+
+.. |tasks_or_file_names| replace:: `tasks_or_file_names`
+.. _tasks_or_file_names: `decorators.collate.tasks_or_file_names`_
+.. |extra_parameters| replace:: `extra_parameters`
+.. _extra_parameters: `decorators.collate.extra_parameters`_
+.. |output_pattern| replace:: `output_pattern`
+.. _output_pattern: `decorators.collate.output_pattern`_
+.. |input_pattern_or_glob| replace:: `input_pattern_or_glob`
+.. _input_pattern_or_glob: `decorators.collate.input_pattern_or_glob`_
+.. |matching_regex| replace:: `matching_regex`
+.. _matching_regex: `decorators.collate.matching_regex`_
+
+
+**********************************************************************************************************************************************************************************************************************************************************************************************************************
+*@collate* ( |tasks_or_file_names|_, :ref:`regex<decorators.regex>`\ *(*\ |matching_regex|_\ *)*\ , [:ref:`inputs<decorators.inputs>`\ *(*\ |input_pattern_or_glob|_\ *)* | :ref:`add_inputs<decorators.add_inputs>`\ *(*\ |input_pattern_or_glob|_\ *)*\] , |output_pattern|_, [|extra_parameters|_,...] )
+**********************************************************************************************************************************************************************************************************************************************************************************************************************
+    **Purpose:**
+        Groups / collates sets of input files, each into a separate summary. 
+        
+        This variant of ``@collate`` allows additional inputs or dependencies to be added 
+        dynamically to the task.
+
+        Output file names are determined from |tasks_or_file_names|_, i.e. from the output
+        of up stream tasks, or a list of file names. 
+
+        This variant of ``@collate`` allows input file names to be derived in the same way.
+    
+        :ref:`add_inputs<decorators.add_inputs>` nests the the original input parameters in a list before adding additional dependencies.
+
+        :ref:`inputs<decorators.inputs>` replaces the original input parameters wholescale.
+
+        Only out of date tasks (comparing input and output files) will be run
+        
+    **Example of** :ref:`add_inputs<decorators.add_inputs>` 
+
+        ``regex(r".*(\..+)"), "\1.summary"`` creates a separate summary file for each suffix.
+        But we also add date of birth data for each species::
+
+            animal_files = "tuna.fish", "shark.fish", "dog.mammals", "cat.mammals"
+            # summarise by file suffix:
+            @collate(animal_files, regex(r".+\.(.+)$"),  add_inputs(r"\1.date_of_birth"), r'\1.summary')
+            def summarize(infiles, summary_file):
+                pass
+
+        This results in the following equivalent function calls::
+
+            summarize([ ["shark.fish",  "fish.date_of_birth"   ], 
+                        ["tuna.fish",   "fish.date_of_birth"   ] ], "fish.summary")
+            summarize([ ["cat.mammals", "mammals.date_of_birth"], 
+                        ["dog.mammals", "mammals.date_of_birth"] ], "mammals.summary")
+    
+    **Example of** :ref:`add_inputs<decorators.inputs>` 
+
+        using ``inputs(...)`` will summarise only the dates of births for each species group::
+
+            animal_files = "tuna.fish", "shark.fish", "dog.mammals", "cat.mammals"
+            # summarise by file suffix:
+            @collate(animal_files, regex(r".+\.(.+)$"),  inputs(r"\1.date_of_birth"), r'\1.summary')
+            def summarize(infiles, summary_file):
+                pass
+
+        This results in the following equivalent function calls::
+
+            summarize(["fish.date_of_birth"   ], "fish.summary")
+            summarize(["mammals.date_of_birth"], "mammals.summary")
+
+    **Parameters:**
+                
+                
+.. _decorators.collate.tasks_or_file_names:
+
+    * *tasks_or_file_names*
+       can be a:
+
+       #.  Task / list of tasks (as in the example above).
+            File names are taken from the output of the specified task(s)
+       #.  (Nested) list of file name strings.
+            File names containing ``*[]?`` will be expanded as a |glob|_.
+             E.g.:``"a.*" => "a.1", "a.2"``
+           
+                
+.. _decorators.collate.matching_regex:
+
+    * *matching_regex*
+       is a python regular expression string, which must be wrapped in
+       a :ref:`regex<decorators.regex>` indicator object
+       See python `regular expression (re) <http://docs.python.org/library/re.html>`_ 
+       documentation for details of regular expression syntax
+                
+.. _decorators.collate.input_pattern_or_glob:
+
+    * *input_pattern*
+       Specifies the resulting input(s) to each job. 
+       Must be wrapped in an :ref:`inputs<decorators.inputs>` or an :ref:`inputs<decorators.add_inputs>` indicator object.
+
+       Can be a:
+
+       #.  Task / list of tasks (as in the example above).
+            File names are taken from the output of the specified task(s)
+       #.  (Nested) list of file name strings.
+            Strings will be subject to (regular expression or suffix) pattern substitution. 
+            File names containing ``*[]?`` will be expanded as a |glob|_.
+            E.g.:``"a.*" => "a.1", "a.2"``
+            
+       
+
+.. _decorators.collate.output_pattern:
+
+    * *output_pattern*
+        Specifies the resulting output file name(s).
+                
+.. _decorators.collate.extra_parameters:
+
+    * *extra_parameters*
+        Any extra parameters are passed verbatim to the task function
+
+    #. *outputs* and optional extra parameters are passed to the functions after regular expression
+       substitution in any strings. Non-string values are passed through unchanged.
+    #. Each collate job consists of input files which are aggregated by regular expression substitution
+       to a single set of output / extra parameter matches
+
+
+See :ref:`@collate <decorators.collate>` for more straightforward ways to use collate.       
---
+++ doc/static_data/example_scripts/play_with_colours.py
@@ -0,0 +1,268 @@
+#!/usr/bin/env python
+"""
+
+    play_with_colours.py
+    [--log_file PATH]
+    [--verbose]
+
+"""
+
+################################################################################
+#
+#   test
+#
+#
+#   Copyright (c) 7/13/2010 Leo Goodstadt
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included in
+#   all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#   THE SOFTWARE.
+#################################################################################
+
+import sys, os
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   options
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+
+from optparse import OptionParser
+import StringIO
+
+parser = OptionParser(version="%play_with_colours 1.0",
+                      usage = "\n\n    play_with_colours "
+                                "--flowchart FILE [options] "
+                                "[--colour_scheme_index INT ] "
+                                "[--key_legend_in_graph]")
+
+#
+#   pipeline
+#
+parser.add_option("--flowchart", dest="flowchart",
+                    metavar="FILE",
+                    type="string",
+                    help="Don't actually run any commands; just print the pipeline "
+                         "as a flowchart.")
+parser.add_option("--colour_scheme_index", dest="colour_scheme_index",
+                    metavar="INTEGER",
+                    type="int",
+                    help="Index of colour scheme for flow chart.")
+parser.add_option("--key_legend_in_graph", dest="key_legend_in_graph",
+                    action="store_true", default=False,
+                    help="Print out legend and key for dependency graph.")
+
+(options, remaining_args) = parser.parse_args()
+if not options.flowchart:
+    raise Exception("Missing mandatory parameter: --flowchart.\n")
+
+
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   imports
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+from ruffus import *
+from ruffus.ruffus_exceptions import JobSignalledBreak
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   Pipeline
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+
+#
+# up to date tasks
+#
+@check_if_uptodate (lambda : (False, ""))
+def Up_to_date_task1(infile, outfile):
+    pass
+
+@check_if_uptodate (lambda : (False, ""))
+@follows(Up_to_date_task1)
+def Up_to_date_task2(infile, outfile):
+    pass
+
+@check_if_uptodate (lambda : (False, ""))
+@follows(Up_to_date_task2)
+def Up_to_date_task3(infile, outfile):
+    pass
+
+
+@check_if_uptodate (lambda : (False, ""))
+@follows(Up_to_date_task3)
+def Up_to_date_final_target(infile, outfile):
+    pass
+
+
+#
+# Explicitly specified
+#
+@check_if_uptodate (lambda : (False, ""))
+@follows(Up_to_date_task1)
+def Explicitly_specified_task(infile, outfile):
+    pass
+
+
+
+#
+# Tasks to run
+#
+@follows(Explicitly_specified_task)
+def Task_to_run1(infile, outfile):
+    pass
+
+
+@follows(Task_to_run1)
+def Task_to_run2(infile, outfile):
+    pass
+
+@follows(Task_to_run2)
+def Task_to_run3(infile, outfile):
+    pass
+
+@check_if_uptodate (lambda : (False, ""))
+@follows(Task_to_run2)
+def Up_to_date_task_forced_to_rerun(infile, outfile):
+    pass
+
+
+#
+# Final target
+#
+@follows(Up_to_date_task_forced_to_rerun, Task_to_run3)
+def Final_target(infile, outfile):
+    pass
+
+#
+# Ignored downstream
+#
+@follows(Final_target)
+def Downstream_task1_ignored(infile, outfile):
+    pass
+
+@follows(Final_target)
+def Downstream_task2_ignored(infile, outfile):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+
+#   Main logic
+
+
+#88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+from collections import defaultdict
+custom_flow_chart_colour_scheme = defaultdict(dict)
+
+#
+#   Base chart on this overall colour scheme index
+#
+custom_flow_chart_colour_scheme["colour_scheme_index"] = options.colour_scheme_index
+
+#
+#   Overriding colours
+#
+if options.colour_scheme_index == None:
+    custom_flow_chart_colour_scheme["Vicious cycle"]["linecolor"]                        = '"#FF3232"'
+    custom_flow_chart_colour_scheme["Pipeline"]["fontcolor"]                             = '"#FF3232"'
+    custom_flow_chart_colour_scheme["Key"]["fontcolor"]                                  = "black"
+    custom_flow_chart_colour_scheme["Key"]["fillcolor"]                                  = '"#F6F4F4"'
+    custom_flow_chart_colour_scheme["Task to run"]["linecolor"]                          = '"#0044A0"'
+    custom_flow_chart_colour_scheme["Up-to-date"]["linecolor"]                           = "gray"
+    custom_flow_chart_colour_scheme["Final target"]["fillcolor"]                         = '"#EFA03B"'
+    custom_flow_chart_colour_scheme["Final target"]["fontcolor"]                         = "black"
+    custom_flow_chart_colour_scheme["Final target"]["color"]                             = "black"
+    custom_flow_chart_colour_scheme["Final target"]["dashed"]                            = 0
+    custom_flow_chart_colour_scheme["Vicious cycle"]["fillcolor"]                        = '"#FF3232"'
+    custom_flow_chart_colour_scheme["Vicious cycle"]["fontcolor"]                        = 'white'
+    custom_flow_chart_colour_scheme["Vicious cycle"]["color"]                            = "white"
+    custom_flow_chart_colour_scheme["Vicious cycle"]["dashed"]                           = 0
+    custom_flow_chart_colour_scheme["Up-to-date task"]["fillcolor"]                      = '"#B8CC6E"'
+    custom_flow_chart_colour_scheme["Up-to-date task"]["fontcolor"]                      = '"#006000"'
+    custom_flow_chart_colour_scheme["Up-to-date task"]["color"]                          = '"#006000"'
+    custom_flow_chart_colour_scheme["Up-to-date task"]["dashed"]                         = 0
+    custom_flow_chart_colour_scheme["Down stream"]["fillcolor"]                          = "white"
+    custom_flow_chart_colour_scheme["Down stream"]["fontcolor"]                          = "gray"
+    custom_flow_chart_colour_scheme["Down stream"]["color"]                              = "gray"
+    custom_flow_chart_colour_scheme["Down stream"]["dashed"]                             = 0
+    custom_flow_chart_colour_scheme["Explicitly specified task"]["fillcolor"]            = "transparent"
+    custom_flow_chart_colour_scheme["Explicitly specified task"]["fontcolor"]            = "black"
+    custom_flow_chart_colour_scheme["Explicitly specified task"]["color"]                = "black"
+    custom_flow_chart_colour_scheme["Explicitly specified task"]["dashed"]               = 0
+    custom_flow_chart_colour_scheme["Task to run"]["fillcolor"]                          = '"#EBF3FF"'
+    custom_flow_chart_colour_scheme["Task to run"]["fontcolor"]                          = '"#0044A0"'
+    custom_flow_chart_colour_scheme["Task to run"]["color"]                              = '"#0044A0"'
+    custom_flow_chart_colour_scheme["Task to run"]["dashed"]                             = 0
+    custom_flow_chart_colour_scheme["Up-to-date task forced to rerun"]["fillcolor"]      = 'transparent'
+    custom_flow_chart_colour_scheme["Up-to-date task forced to rerun"]["fontcolor"]      = '"#0044A0"'
+    custom_flow_chart_colour_scheme["Up-to-date task forced to rerun"]["color"]          = '"#0044A0"'
+    custom_flow_chart_colour_scheme["Up-to-date task forced to rerun"]["dashed"]         = 1
+    custom_flow_chart_colour_scheme["Up-to-date Final target"]["fillcolor"]              = '"#EFA03B"'
+    custom_flow_chart_colour_scheme["Up-to-date Final target"]["fontcolor"]              = '"#006000"'
+    custom_flow_chart_colour_scheme["Up-to-date Final target"]["color"]                  = '"#006000"'
+    custom_flow_chart_colour_scheme["Up-to-date Final target"]["dashed"]                 = 0
+
+if __name__ == '__main__':
+    pipeline_printout_graph (
+
+                                    open(options.flowchart, "w"),
+                                    # use flowchart file name extension to decide flowchart format
+                                    #   e.g. svg, jpg etc.
+                                    os.path.splitext(options.flowchart)[1][1:],
+
+                                    # final targets
+                                    [Final_target, Up_to_date_final_target],
+
+                                    # Explicitly specified tasks
+                                    [Explicitly_specified_task],
+
+                                    # Do we want key legend
+                                    no_key_legend   = not options.key_legend_in_graph,
+
+                                    # Print all the task types whether used or not
+                                    minimal_key_legend             = False,
+
+                                    user_colour_scheme = custom_flow_chart_colour_scheme,
+                                    pipeline_name = "Colour schemes")
+
+
+
+
+
+
+
+
