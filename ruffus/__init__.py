9555c2821c2a51c4b7c62d8653691ed3d0dc5da9
Leo Goodstadt <src@llew.org.uk>
2010-07-21 21:30:05 +0000

    add imports
    --HG--
    extra : convert_revision : svn%3Aa4fb1d42-34dc-11de-8ef0-0f53c1b7e458/trunk%40285

--- ruffus/__init__.py
+++ ruffus/__init__.py
@@ -5,17 +5,17 @@
 #
 #
 #   Copyright (c) 10/9/2009 Leo Goodstadt
-#   
+#
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
 #   in the Software without restriction, including without limitation the rights
 #   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 #   copies of the Software, and to permit persons to whom the Software is
 #   furnished to do so, subject to the following conditions:
-#   
+#
 #   The above copyright notice and this permission notice shall be included in
 #   all copies or substantial portions of the Software.
-#   
+#
 #   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,7 +27,10 @@
 #from graph import *
 #from print_dependencies import *
 from task import pipeline_printout, pipeline_printout_graph, pipeline_run, register_cleanup, check_if_uptodate, split, transform, merge, collate, files, files_re, follows, parallel, stderr_logger, black_hole_logger, suffix, regex, inputs, add_inputs, touch_file, combine, mkdir, output_from, posttask, JobSignalledBreak, runtime_parameter, jobs_limit
-#output_dependency_tree_in_dot_format, output_dependency_tree_key_in_dot_format 
+from graph  import graph_colour_demo_printout
+from file_name_parameters import needs_update_check_modify_time
+
+#output_dependency_tree_in_dot_format, output_dependency_tree_key_in_dot_format
 import ruffus_version
 __version__=ruffus_version.__version
 
