ffa58d401ac579f3efa4ef403a77b7aac825b261
Leo Goodstadt <src@llew.org.uk>
2013-11-22 16:37:15 +0000

    refactor unit test to make it more unit testy

--- ruffus/test/test_split_regex_and_collate.py
+++ ruffus/test/test_split_regex_and_collate.py
@@ -3,17 +3,6 @@
 
     branching.py
 
-        test branching dependencies
-
-        use :
-            --debug               to test automatically
-            --start_again         the first time you run the file
-            --jobs_per_task N     to simulate tasks with N numbers of files per task
-
-            -j N / --jobs N       to speify multitasking
-            -v                    to see the jobs in action
-            -n / --just_print     to see what jobs would run
-
 """
 
 
@@ -27,86 +16,26 @@
 from optparse import OptionParser
 import sys, os
 import os.path
-import StringIO
+from StringIO import StringIO
 import re,time
+import operator
+from collections import defaultdict
+import random
+import shutil
 
-# add self to search path for testing
 exe_path = os.path.split(os.path.abspath(sys.argv[0]))[0]
-sys.path.insert(0,os.path.abspath(os.path.join(exe_path,"..", "..")))
-if __name__ == '__main__':
-    module_name = os.path.split(sys.argv[0])[1]
-    module_name = os.path.splitext(module_name)[0];
-else:
-    module_name = __name__
+sys.path.insert(0, os.path.abspath(os.path.join(exe_path,"..", "..")))
+from ruffus import *
+from ruffus import (pipeline_run, pipeline_printout, suffix, transform, split,
+                    merge, dbdict, follows)
+from ruffus.combinatorics import *
+from ruffus.ruffus_exceptions import RethrownJobError
+from ruffus.ruffus_utility import (RUFFUS_HISTORY_FILE,
+                                   CHECKSUM_FILE_TIMESTAMPS)
 
+import unittest
 
-
-import ruffus
-parser = OptionParser(version="%%prog v1.0, ruffus v%s" % ruffus.ruffus_version.__version)
-parser.add_option("-D", "--debug", dest="debug",
-                    action="store_true", default=False,
-                    help="Make sure output is correct and clean up.")
-parser.add_option("-s", "--start_again", dest="start_again",
-                    action="store_true", default=False,
-                    help="Make a new 'original.fa' file to simulate having to restart "
-                            "pipeline from scratch.")
-parser.add_option("--jobs_per_task", dest="jobs_per_task",
-                      default=2,
-                      metavar="N",
-                      type="int",
-                      help="Simulates tasks with N numbers of files per task.")
-
-
-parser.add_option("-t", "--target_tasks", dest="target_tasks",
-                  action="append",
-                  default = list(),
-                  metavar="JOBNAME",
-                  type="string",
-                  help="Target task(s) of pipeline.")
-parser.add_option("-f", "--forced_tasks", dest="forced_tasks",
-                  action="append",
-                  default = list(),
-                  metavar="JOBNAME",
-                  type="string",
-                  help="Pipeline task(s) which will be included even if they are up to date.")
-parser.add_option("-j", "--jobs", dest="jobs",
-                  default=1,
-                  metavar="jobs",
-                  type="int",
-                  help="Specifies  the number of jobs (commands) to run simultaneously.")
-parser.add_option("-v", "--verbose", dest = "verbose",
-                  action="count", default=0,
-                  help="Print more verbose messages for each additional verbose level.")
-parser.add_option("-d", "--dependency", dest="dependency_file",
-                  #default="simple.svg",
-                  metavar="FILE",
-                  type="string",
-                  help="Print a dependency graph of the pipeline that would be executed "
-                        "to FILE, but do not execute it.")
-parser.add_option("-F", "--dependency_graph_format", dest="dependency_graph_format",
-                  metavar="FORMAT",
-                  type="string",
-                  default = 'svg',
-                  help="format of dependency graph file. Can be 'ps' (PostScript), "+
-                  "'svg' 'svgz' (Structured Vector Graphics), " +
-                  "'png' 'gif' (bitmap  graphics) etc ")
-parser.add_option("-n", "--just_print", dest="just_print",
-                    action="store_true", default=False,
-                    help="Print a description of the jobs that would be executed, "
-                        "but do not execute them.")
-parser.add_option("-M", "--minimal_rebuild_mode", dest="minimal_rebuild_mode",
-                    action="store_true", default=False,
-                    help="Rebuild a minimum of tasks necessary for the target. "
-                    "Ignore upstream out of date tasks if intervening tasks are fine.")
-parser.add_option("-K", "--no_key_legend_in_graph", dest="no_key_legend_in_graph",
-                    action="store_true", default=False,
-                    help="Do not print out legend and key for dependency graph.")
-parser.add_option("-H", "--draw_graph_horizontally", dest="draw_horizontally",
-                    action="store_true", default=False,
-                    help="Draw horizontal dependency graph.")
-
-parameters = [
-                ]
+JOBS_PER_TASK = 5
 
 
 
@@ -121,16 +50,6 @@ parameters = [
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
-import StringIO
-import re
-import operator
-import sys,os
-from collections import defaultdict
-import random
-
-sys.path.append(os.path.abspath(os.path.join(exe_path,"..", "..")))
-from ruffus import *
-
 # use simplejson in place of json for python < 2.6
 try:
     import json
@@ -144,32 +63,15 @@ except ImportError:
 
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
-
-
-
-
-
-# get help string
-f =StringIO.StringIO()
-parser.print_help(f)
-helpstr = f.getvalue()
-(options, remaining_args) = parser.parse_args()
-
-
 tempdir = "temp_filesre_split_and_combine/"
-
-
-
-if options.verbose:
-    verbose_output = sys.stderr
-else:
-    verbose_output =open("/dev/null", "w")
-
 
 #
 #   Three starting files
 #
 original_files = [tempdir  + "original_%d.fa" % d for d in range(3)]
+
+
+
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 
@@ -177,12 +79,19 @@ original_files = [tempdir  + "original_%d.fa" % d for d in range(3)]
 
 
 #88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
+@originate(original_files)
+def generate_initial_files(out_names):
+    for on in out_names:
+        with open(on, 'w') as outfile:
+            pass
+
+
 #
 #    split_fasta_file
 #
 
-@posttask(lambda: verbose_output.write("Split into %d files each\n" % options.jobs_per_task))
-@split(original_files,
+@posttask(lambda: sys.stderr.write("Split into %d files each\n" % JOBS_PER_TASK))
+@subdivide(generate_initial_files,
        regex(r".*\/original_(\d+).fa"),                               # match original files
        [tempdir + r"files.split.\1.success",                         # flag file for each original file
         tempdir + r"files.split.\1.*.fa"],                           # glob pattern
@@ -198,9 +107,9 @@ def split_fasta_file (input_file, outputs, original_index):
         os.unlink(f)
 
     #
-    # create as many files as we are simulating in jobs_per_task
+    # create as many files as we are simulating in JOBS_PER_TASK
     #
-    for i in range(options.jobs_per_task):
+    for i in range(JOBS_PER_TASK):
         open(tempdir + "files.split.%s.%03d.fa" % (original_index, i), "w")
 
     open(success_flag,  "w")
@@ -210,7 +119,7 @@ def split_fasta_file (input_file, outputs, original_index):
 #
 #    align_sequences
 #
-@posttask(lambda: verbose_output.write("Sequences aligned\n"))
+@posttask(lambda: sys.stderr.write("Sequences aligned\n"))
 @transform(split_fasta_file, suffix(".fa"), ".aln")                     # fa -> aln
 def align_sequences (input_file, output_filename):
     open(output_filename, "w").write("%s\n" % output_filename)
@@ -221,7 +130,7 @@ def align_sequences (input_file, output_filename):
 #
 #    percentage_identity
 #
-@posttask(lambda: verbose_output.write("%Identity calculated\n"))
+@posttask(lambda: sys.stderr.write("%Identity calculated\n"))
 @transform(align_sequences,             # find all results from align_sequences
             suffix(".aln"),             # replace suffix with:
             [r".pcid",                  #   .pcid suffix for the result
@@ -237,7 +146,7 @@ def percentage_identity (input_file, output_files):
 #
 #    combine_results
 #
-@posttask(lambda: verbose_output.write("Results recombined\n"))
+@posttask(lambda: sys.stderr.write("Results recombined\n"))
 @collate(percentage_identity, regex(r".*files.split\.(\d+)\.\d+.pcid"),
          [tempdir + r"\1.all.combine_results",
           tempdir + r"\1.all.combine_results_success"])
@@ -253,44 +162,46 @@ def combine_results (input_files, output_files):
 
 
 
-def start_pipeline_afresh ():
-    """
-    Recreate directory and starting file
-    """
-    print >>verbose_output, "Start again"
-    import os
-    os.system("rm -rf %s" % tempdir)
-    os.makedirs(tempdir)
-    for f in original_files:
-        open(f, "w").close()
 
+class Test_split_regex_and_collate(unittest.TestCase):
+    def setUp(self):
+        import os
+        shutil.rmtree(tempdir)
+        os.makedirs(tempdir)
+        for f in original_files:
+            print f
+            with open(f, "w") as p: pass
+
+    def cleanup_tmpdir(self):
+        os.system('rm -f %s %s' % (os.path.join(tempdir, '*'), RUFFUS_HISTORY_FILE))
+
+    #___________________________________________________________________________
+    #
+    #   test product() pipeline_printout and pipeline_run
+    #___________________________________________________________________________
+    def test_collate(self):
+        self.cleanup_tmpdir()
+
+        s = StringIO()
+        pipeline_printout(s, [combine_results], verbose=5, wrap_width = 10000)
+        self.assertIn('Job needs update: Missing files ', s.getvalue())
+        print s.getvalue()
+
+        pipeline_run([combine_results])
+
+    #___________________________________________________________________________
+    #
+    #   cleanup
+    #___________________________________________________________________________
+    def tearDown(self):
+        return
+        shutil.rmtree(tempdir)
+
+#
+#   Necessary to protect the "entry point" of the program under windows.
+#       see: http://docs.python.org/library/multiprocessing.html#multiprocessing-programming
+#
 if __name__ == '__main__':
-    if options.start_again:
-        start_pipeline_afresh()
-    if options.just_print:
-        pipeline_printout(sys.stdout, options.target_tasks, options.forced_tasks,
-                            verbose = options.verbose,
-                            gnu_make_maximal_rebuild_mode = not options.minimal_rebuild_mode)
-
-    elif options.dependency_file:
-        pipeline_printout_graph (     open(options.dependency_file, "w"),
-                             options.dependency_graph_format,
-                             options.target_tasks,
-                             options.forced_tasks,
-                             draw_vertically = not options.draw_horizontally,
-                             gnu_make_maximal_rebuild_mode  = not options.minimal_rebuild_mode,
-                             no_key_legend  = options.no_key_legend_in_graph)
-    elif options.debug:
-        start_pipeline_afresh()
-        pipeline_run(options.target_tasks, options.forced_tasks, multiprocess = options.jobs,
-                            logger = stderr_logger if options.verbose else black_hole_logger,
-                            gnu_make_maximal_rebuild_mode  = not options.minimal_rebuild_mode,
-                            verbose = options.verbose)
-        os.system("rm -rf %s" % tempdir)
-        print "OK"
-    else:
-        pipeline_run(options.target_tasks, options.forced_tasks, multiprocess = options.jobs,
-                            logger = stderr_logger if options.verbose else black_hole_logger,
-                             gnu_make_maximal_rebuild_mode  = not options.minimal_rebuild_mode,
-                            verbose = options.verbose)
+    #pipeline_printout(sys.stdout, [test_product_task], verbose = 5)
+    unittest.main()
 
