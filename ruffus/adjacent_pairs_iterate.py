1f03df6fcbbcaf7d5e872528b2011ba224c2ddb6
Leo Goodstadt <src@llew.org.uk>
2014-07-21 16:40:21 +0100

    python 3 changes

--- ruffus/adjacent_pairs_iterate.py
+++ ruffus/adjacent_pairs_iterate.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python
+from __future__ import print_function
+from future_builtins import zip
 ################################################################################
 #
 #   adjacent_pairs_iterate.py
 #
 #
 #   Copyright (c) 2007 Leo Goodstadt
-#   
+#
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
 #   in the Software without restriction, including without limitation the rights
 #   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 #   copies of the Software, and to permit persons to whom the Software is
 #   furnished to do so, subject to the following conditions:
-#   
+#
 #   The above copyright notice and this permission notice shall be included in
 #   all copies or substantial portions of the Software.
-#   
+#
 #   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 #   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 #   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -26,36 +28,35 @@
 #################################################################################
 
 
-from itertools import izip
 def adjacent_pairs_iterate (array, reverse = False):
     """
     returns pairs of iterators to successive positions
     """
     if len(array) < 2:
         return
-       
+
     # get iterators to successive positions
     if reverse:
-        curr = reversed(array)
-        next = reversed(array)
+        curr_iter = reversed(array)
+        next_iter = reversed(array)
     else:
-        curr = iter(array)
-        next = iter(array)
-    next.next() 
-    for i, j in izip(curr, next):
-        yield i, j    
-    
+        curr_iter = iter(array)
+        next_iter = iter(array)
+    next(next_iter)
+    for i, j in zip(curr_iter, next_iter):
+        yield i, j
+
 
 def unit_test():
-    numbers = range(10)
-    print "Forward"
+    numbers = list(range(10))
+    print("Forward")
     for i, j in adjacent_pairs_iterate(numbers):
-        print i, j
-    print "Reversed"
+        print(i, j)
+    print("Reversed")
     for i, j in adjacent_pairs_iterate(numbers, reverse=True):
-        print i, j
-    print numbers
-        
+        print(i, j)
+    print(numbers)
+
 if __name__ == '__main__':
     unit_test()
 
