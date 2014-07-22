d8430fafb372f139f9367cbb2f1c0bfcb461a224
Leo Goodstadt <src@llew.org.uk>
2014-07-22 02:38:52 +0100

    changes to ensure python3 compatability

--- ruffus/test/test_split_regex_and_collate.py
+++ ruffus/test/test_split_regex_and_collate.py
@@ -196,7 +196,7 @@ class Test_split_regex_and_collate(unittest.TestCase):
 
         s = StringIO()
         pipeline_printout(s, [combine_results], verbose=5, wrap_width = 10000)
-        self.assertIn('Job needs update: Missing files ', s.getvalue())
+        self.assertTrue('Job needs update: Missing files ' in s.getvalue())
         #print s.getvalue()
 
         pipeline_run([combine_results], verbose=0)
