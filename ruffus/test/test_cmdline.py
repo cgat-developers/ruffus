f321d3031c9308d0b3071cd11954ff1aa7c9ae20
Leo Goodstadt <src@llew.org.uk>
2014-07-24 22:19:35 +0100

    ensure python 2.6 compatibility by backporting bits borrowed from standard unittest module

--- ruffus/test/test_cmdline.py
+++ ruffus/test/test_cmdline.py
@@ -32,42 +32,9 @@ class t_options(object):
     def __repr__(self):
         return str(self.__dict__)
 
-class _AssertRaisesContext_v27(object):
-    """A context manager used to implement TestCase.assertRaises* methods."""
-
-    def __init__(self, expected, test_case, expected_regexp=None):
-        self.expected = expected
-        self.failureException = test_case.failureException
-        self.expected_regexp = expected_regexp
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, tb):
-        if exc_type is None:
-            try:
-                exc_name = self.expected.__name__
-            except AttributeError:
-                exc_name = str(self.expected)
-            raise self.failureException(
-                "{0} not raised".format(exc_name))
-        if not issubclass(exc_type, self.expected):
-            # let unexpected exceptions pass through
-            return False
-        self.exception = exc_value # store for later retrieval
-        if self.expected_regexp is None:
-            return True
-
-        expected_regexp = self.expected_regexp
-        if isinstance(expected_regexp, basestring):
-            expected_regexp = re.compile(expected_regexp)
-        if not expected_regexp.search(str(exc_value)):
-            raise self.failureException('"%s" does not match "%s"' %
-                     (expected_regexp.pattern, str(exc_value)))
-        return True
 
 class Test_cmdline(unittest.TestCase):
-    def setUp(self):
+    #def setUp(self):
         #if sys.hexversion < 0x03000000:
         #    self.assertRaisesRegex = self.assertRaisesRegexp
 
