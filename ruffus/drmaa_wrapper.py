33959c9328ddd5074fe06f93b4f4f3ebf70e3fb9
Leo Goodstadt <src@llew.org.uk>
2014-07-30 21:31:02 +0100

    Bug fix: still Ctrl C issues

--- ruffus/drmaa_wrapper.py
+++ ruffus/drmaa_wrapper.py
@@ -368,8 +368,7 @@ def run_job_locally (cmd_str, logger = None):
                                  shell = True,
                                  stdin = subprocess.PIPE,
                                  stdout = subprocess.PIPE
-                                 )
-                                 #,stderr = subprocess.PIPE )
+                                 stderr = subprocess.PIPE )
 
     # process.stdin.close()
     stdout, stderr = process.communicate()
