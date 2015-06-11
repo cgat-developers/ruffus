8b1fb5c3911b8d21f4f60c6a9c80ef92c9920625
Leo Goodstadt <src@llew.org.uk>
2015-06-11 19:02:03 +0100

    Work around python 2.6 unittest.TestCase doesn't have assertNotRegexpMatches

--- ruffus/test/test_check_if_uptodate.py
+++ ruffus/test/test_check_if_uptodate.py
@@ -41,15 +41,6 @@ try:
     from StringIO import StringIO
 except:
     from io import StringIO
-from collections import defaultdict
-
-# use simplejson in place of json for python < 2.6
-import json
-#try:
-#    import json
-#except ImportError:
-#    import simplejson
-#    json = simplejson
 
 
 
@@ -76,11 +67,19 @@ def task2(x):
 
 
 
-
-
-
-#
 class Test_ruffus(unittest.TestCase):
+
+    def do_assertNotRegexpMatches (self, test_str, re_str):
+        import re
+        if re.search(re_str, test_str) is not None:
+            raise AssertionError("Regexp matched: %r found in %r" % (re_str, test_str))
+
+    def do_assertRegexpMatches (self, test_str, re_str):
+        import re
+        if re.search(re_str, test_str) is None:
+            #AssertionError: 
+            raise AssertionError("Regexp didn't match: %r not found in %r" % (re_str, test_str))
+
     def setUp(self):
         try:
             shutil.rmtree(tempdir)
@@ -103,13 +102,18 @@ class Test_ruffus(unittest.TestCase):
 
         ret = s.getvalue()
         try:
-            self.assertNotRegexpMatches(ret, r"Jobs needs update:\s*No function to check if up-to-date")
+            self.do_assertRegexpMatches(ret, r"Tasks which are up-to-date:(\n\s*)*Task = 'test_check_if_uptodate.task1'(\n\s*)*Task = 'test_check_if_uptodate.task2'")
         except:
-            print ("Oops: @check_if_uptodate is not being picked up\n")
+            print ("\n\tOops: Both tasks should be up to date!!\n\n")
             raise
-        self.assertRegexpMatches(ret, r"Tasks which are up-to-date:(\n\s*)*Task = 'test_check_if_uptodate.task1'(\n\s*)*Task = 'test_check_if_uptodate.task2'")
+        try:
+            self.do_assertNotRegexpMatches(ret, r"Jobs needs update:\s*No function to check if up-to-date")
+        except:
+            print ("\n\tOops: @check_if_uptodate is not being picked up!!\n\n")
+            raise
 
 
 if __name__ == '__main__':
     unittest.main()
+
 
