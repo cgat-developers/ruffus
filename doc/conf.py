94087b6492fa50d6bf2f3098f2b1c5b93b9abc4b
Andreas Tille <tille@debian.org>
2016-07-25 13:19:50 +0200

    Fix sphinx.ext.mathjax: other math package is already loaded

--- doc/conf.py
+++ doc/conf.py
@@ -36,7 +36,7 @@ def setup(app):
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-              'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+              'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
