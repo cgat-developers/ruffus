67752ad3d15e617d2fcc4c818a8dcb72c1ca2e06
Andreas Heger <andreas.heger@gmail.com>
2019-08-12 09:50:50 +0100

    {AH} add missing import

--- ruffus/file_name_parameters.py
+++ ruffus/file_name_parameters.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
 import re
-import time
 from . import dbdict
 from .ruffus_utility import *
 from .ruffus_utility import shorten_filenames_encoder, FILE_CHECK_RETRY, FILE_CHECK_SLEEP
@@ -56,6 +55,7 @@ from .ruffus_exceptions import *
 # 88888888888888888888888888888888888888888888888888888888888888888888888888888888888888888
 import os
 import sys
+import time
 import glob
 from itertools import groupby
 import itertools
@@ -95,13 +95,11 @@ def epoch_seconds_to_str(epoch_seconds):
     """
     #   returns 24 char long  25 May 2011 23:37:40.12
     time_str = strftime("%d %b %Y %H:%M:%S", gmtime(epoch_seconds))
-import time
     #
     fraction_of_second_as_str = (
         "%.2f" % (epoch_seconds - int(epoch_seconds)))[1:]
     #   or fraction = ("%.2f" % (divmod(epoch_seconds, 1)[1]))[1:]
     return (time_str + fraction_of_second_as_str)
-import time
 
 
 err_msg_no_regex_match = ("No jobs were run because no file names matched.\n"
